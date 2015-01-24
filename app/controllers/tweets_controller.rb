029a483b12c08b3d211fd6cb6a813f727262dc37
Cynthia <cynthiasschen@gmail.com>
2015-01-24 17:59:43 -0500

    added user authentication

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,19 @@
 class TweetsController < ApplicationController
 #convention to use the same instance variable name
+
+#do not let a not signed in user access the page, ! means throw an error if it fails
+	before_action :authenticate_user!
+
 	def new
 		@tweet = Tweet.new
 	end
 
 	def create
-		@tweet = Tweet.create(tweet_params)
+
+		#tweet.create is short for tweet.new and tweet.save
+		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
+		@tweet.save
 		#flash is a built in hash
 		flash.now[:success] = "Tweet Created"
 		#render new.html.erb page
