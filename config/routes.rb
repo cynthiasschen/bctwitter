e9a8c5f734398b892b36856fc834435d8e44fb7a
Cynthia <cynthiasschen@gmail.com>
2015-01-24 16:58:18 -0500

    Created signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'tweets#new'
+   
+   root 'static_pages#index'
 
    #for the tweets controller, add all resources associated with CRUD 
    resources :tweets
